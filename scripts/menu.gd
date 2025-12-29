21e05837f494d3286087af08ab4e326ac7c127e8
ditari <xiratid@gmail.com>
2025-12-29 07:06:20 +0700

    menu and title

---
+++ scripts/menu.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+
+
+func _on_button_pressed():
+	if Global.audio_settings["sounds"]:
+		AudioController.play_button()	
+	
+	await get_tree().create_timer(0.3).timeout	
+	get_tree().change_scene_to_file("res://scenes/levelchoice.tscn")
