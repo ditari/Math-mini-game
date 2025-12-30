7e190b3e5d7a86686d31db36263f6839b087e676
ditari <xiratid@gmail.com>
2025-12-30 07:17:09 +0700

    credits

---
+++ scripts/credits.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_button_pressed():
+	if Global.audio_settings["sounds"]:
+		AudioController.play_button()
+
+	await get_tree().create_timer(0.3).timeout
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,15 +7,12 @@ func _on_button_pressed():
 	await get_tree().create_timer(0.3).timeout	
 	get_tree().change_scene_to_file("res://scenes/levelchoice.tscn")
 
-
 func _on_button_3_pressed():
 	if Global.audio_settings["sounds"]:
 		AudioController.play_button()	
 	
 	await get_tree().create_timer(0.3).timeout	
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
-	
-
 
 func _on_button_2_pressed():
 	if Global.audio_settings["sounds"]:
@@ -24,3 +21,11 @@ func _on_button_2_pressed():
 	await get_tree().create_timer(0.3).timeout	
 	get_tree().change_scene_to_file("res://scenes/highscore.tscn")
 		
+
+func _on_button_4_pressed():
+	if Global.audio_settings["sounds"]:
+		AudioController.play_button()	
+	
+	await get_tree().create_timer(0.3).timeout	
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+	
